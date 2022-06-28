d71a1649b8274f22db1d9c92aa26414e95568bdc
hyun-hyang <jimin3060@naver.com>
2022-06-29 00:13:49 +0900

    item, itemlist 구현중

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,67 @@
 
 public class Item {
-	String name;
-	int cost;
+	private String classification; //항목분류
+	private String menuName; //메뉴
+	private int cost; //가격
+	private boolean soldoutStatus; //품절여부 (초기값 false)
+	
+	public Item(String classification, String menuName, int cost) {
+		this.setClassification(classification);
+		this.setMenuName(menuName);
+		this.setCost(cost);
+		//soloutStatus 초기값은 false이므로 굳이 주지않았다면 그대로 둔다. 
+	}
+	
+	public Item(String classification, String menuName, int cost, boolean soldoutStatus) {
+		this.setClassification(classification);
+		this.setMenuName(menuName);
+		this.setCost(cost);
+		this.setSoldoutStatus(soldoutStatus);
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public String getClassification() {
+		return classification;
+	}
+
+	public void setClassification(String classification) {
+		/* 올바른 항목분류인지 확인후 set해준다 */
+		if (ItemList.checkClassification(classification)) {
+			this.classification = classification;
+		}
+	}
+
+	public boolean isSoldoutStatus() {
+		return soldoutStatus;
+	}
+	
+	public void setSoldoutStatus(String soldoutStatus) {
+		if(soldoutStatus.equals("품절")) {
+			setSoldoutStatus(true);
+		}
+	}
+
+	public void setSoldoutStatus(boolean soldoutStatus) {
+		this.soldoutStatus = soldoutStatus;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		/* 0원 이상일 경우 set해준다 */
+		if (cost>=0) {
+			this.cost = cost;
+		}
+	}
+	
+	
 }
---
+++ src/ItemList.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ItemList {
+	ArrayList<Item> itemList;
+	static ArrayList<String> classificationList;
+	File sourceFile;
+	
+	public ItemList() {
+		ArrayList<Item> itemList = new ArrayList<> ();
+		ArrayList<String> classificationList = new ArrayList<>(Arrays.asList("커피", "콜드브루", "논 커피", "티/에이드", "프라페/블렌디드", "병음료", "디저트"));
+
+		classificationList.add("커피");
+		fileload();
+		printItemList();
+	}
+	
+	public static boolean checkClassification (String newclassification) {
+		/* classification이 맞게 들어왔나 확인하는 메서드
+		 * 만약 올바르다면 true를 리턴, 항목분류리스트에 없다면 false를 리턴*/
+		
+		
+		if (classificationList.contains(newclassification)) {
+			return true;
+		} else {
+			System.out.println("Wrong Classification! Please Check it again.");
+			return false;
+		}
+	
+	}
+	
+	public void fileload() {
+		File sourceFile = new File("메뉴리스트.txt");
+		try {
+			BufferedReader bis = new BufferedReader(new FileReader(sourceFile));
+			String lineString = "";
+			String[] array = new String[4];
+			while((lineString = bis.readLine()) != null) {
+				array = lineString.split(",", 4);
+				for(int i = 0; i < array.length ; i++) {
+					array[i].trim();
+					if ( (array[i] == null) && (array[i].isBlank()) ){
+						array[i] = "";
+					}
+				}
+				if(!checkClassification(array[0])) continue;
+				itemList.add(new Item(array[0], array[1], Integer.parseInt(array[2]), Boolean.parseBoolean(array[3])));
+			}
+			bis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void printItemList() {
+		File targetFile = new File("gui용 메뉴리스트.txt");
+		try {
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(targetFile));
+			objectOutputStream.writeObject(itemList);
+			System.out.println(itemList);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public int getNumOfItemList() {
+		return itemList.size();
+	}
+
+}
