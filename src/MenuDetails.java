3b05f84f21bb18fc5cc399f51bcfc3555a59ab97
pch4947 <pch9339@gmail.com>
2022-07-01 19:45:46 +0900

    각 옵션에 따라 가격 변동 추가

--- src/MenuDetails.java
+++ src/MenuDetails.java
@@ -6,7 +6,6 @@ public class MenuDetails extends Item{
 	private boolean addWhippedCream; // 휘핑 추가
 	private boolean isDecaffeinated; // 디카페인
 	private boolean isTumbler; //텀블러 여부
-	private int cost;
 	
 	public MenuDetails(String classification, String menuName, int cost, boolean isICE, boolean sizeUp, boolean extraShot, boolean addWhippedCream, boolean isTumbler){ // 커피류
 		super(classification, menuName, cost);
@@ -33,18 +32,22 @@ public class MenuDetails extends Item{
 	
 	public void setExtraShot(boolean extraShot) {
 		this.extraShot = extraShot;
+		if(this.extraShot) this.cost += 300;
 	}
 	
 	public void setAddWhippedCream(boolean addWhippedCream) {
 		this.addWhippedCream = addWhippedCream;
+		if(this.addWhippedCream) this.cost += 300;
 	}
 	
 	public void setIsDecaffeinated(boolean isDecaffeinated) {
 		this.isDecaffeinated = isDecaffeinated;
+		if(this.isDecaffeinated) this.cost += 500;
 	}
 	
 	public void setIsTumbler(boolean isTumbler) {
 		this.isTumbler = isTumbler;
+		if(this.isTumbler) this.cost -= 300;
 	}
 	
 	public boolean getIsICE() {
