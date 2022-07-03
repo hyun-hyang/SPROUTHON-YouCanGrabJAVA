bb94c90104f23759f3cab5e1079222ae3db74839
pch4947 <pch9339@gmail.com>
2022-07-03 21:51:53 +0900

    TeaAde super 완료

--- src/machine/TeaAde.java
+++ src/machine/TeaAde.java
@@ -2,6 +2,10 @@ package machine;
 
 public class TeaAde extends MenuDetails{
 	
+	public TeaAde(String classification, String menuName, int cost) {
+		super(classification, menuName, cost);
+	}
+	
 	public TeaAde(String classification, String menuName, int cost, boolean isICE, boolean sizeUp, boolean isTumbler) {
 		super(classification, menuName, cost);
 		this.setIsICE(isICE);
