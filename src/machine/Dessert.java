c3b7ab7bc1ff0175dd95f328a57dc9e1a0a30582
pch4947 <pch9339@gmail.com>
2022-07-03 21:53:04 +0900

    Dessert super 완료

--- src/machine/Dessert.java
+++ src/machine/Dessert.java
@@ -1,6 +1,10 @@
 package machine;
 
 public class Dessert extends MenuDetails {
+	
+	public Dessert(String classification, String menuName, int cost) {
+		super(classification, menuName, cost);
+	}
 
 	public Dessert(String classification, String menuName, int cost, boolean isICE, boolean sizeUp, boolean isTumbler) {
 		super(classification, menuName, cost);
