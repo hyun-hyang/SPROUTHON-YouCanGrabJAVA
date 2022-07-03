d1c16330d2d48fd8af0149dc95df71ba90429da7
pch4947 <pch9339@gmail.com>
2022-07-03 21:50:38 +0900

    Coldbrew super 완료

--- src/machine/Coldbrew.java
+++ src/machine/Coldbrew.java
@@ -2,6 +2,10 @@ package machine;
 
 public class Coldbrew extends MenuDetails {
 	
+	public Coldbrew(String classification, String menuName, int cost) {
+		super(classification, menuName, cost);
+	}
+	
 	public Coldbrew(String classification, String menuName, int cost, boolean isICE, boolean sizeUp, boolean isDecaffeinated, boolean isTumbler) {
 		super(classification, menuName, cost);
 		this.setIsICE(isICE);
