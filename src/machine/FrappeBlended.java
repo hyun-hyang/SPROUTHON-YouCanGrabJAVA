be98fc7bfb40800cff98253118fe2e3d7cad2917
pch4947 <pch9339@gmail.com>
2022-07-03 21:52:30 +0900

    FrappeBlended super 완료

--- src/machine/FrappeBlended.java
+++ src/machine/FrappeBlended.java
@@ -1,6 +1,10 @@
 package machine;
 
 public class FrappeBlended extends MenuDetails {
+	
+	public FrappeBlended(String classification, String menuName, int cost) {
+		super(classification, menuName, cost);
+	}
 
 	public FrappeBlended(String classification, String menuName, int cost, boolean sizeUp, boolean isTumbler) {
 		super(classification, menuName, cost);
